d9a4c4ddd482c1178c4b52e0b82e9143491c5a49
Aman Kansal <amankansal.india@gmail.com>
2018-11-24 01:55:50 +0530

    zenity working

--- Linux_Client/notify.sh
+++ Linux_Client/notify.sh
@@ -3,11 +3,12 @@ export DISPLAY=:0.0
 spc --status | sed -n '/^Files wi/,/^--/p' > ~/difftemp
 scheme=$(DISPLAY=:0.0 /usr/bin/zenity --entry  --text="You may want to sync your files as a matter of regularity.\n Choose\n 1 for mirroring files on server\n 2 for merging and overwriting on server\n 3 for overwriting on client.\n Cancel if you don't want to sync")
 if [ $scheme = 1 ];then
-	/usr/bin/spc --sync < 1
+	echo 34
+	echo 1 | /usr/bin/spc --sync
 fi
 if [ $scheme = 2 ];then
-	/usr/bin/spc --sync < 2
+	echo 2 | /usr/bin/spc --sync
 fi
 if [ $scheme = 3 ];then
-	/usr/bin/spc --sync < 3
+	echo 3 | /usr/bin/spc --sync
 fi
