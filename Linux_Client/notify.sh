4eb1bad3b78f39688661347af75eddf0ab028d26
AnshKhurana <anshkhurana1507@gmail.com>
2018-11-23 21:49:02 +0530

    Working on exceptions

--- Linux_Client/notify.sh
+++ Linux_Client/notify.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export DISPLAY=:0.0
-spc --status | sed -n '/^Files wi/,/^--/p' > /usr/bin/difftemp
-DISPLAY=:0.0 /usr/bin/notify-send  "You have not been on Cloud it seems!" "`cat /usr/bin/difftemp`"
+spc --status | sed -n '/^Files wi/,/^--/p' > ~/difftemp
+DISPLAY=:0.0 /usr/bin/notify-send  "You have not been on Cloud it seems!" "`cat ~/difftemp`"
